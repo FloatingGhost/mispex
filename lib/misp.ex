038ee963635759390b5e12c0c2bba7776cabf842
Hannah Ward <Hannah.ward9001@gmail.com>
2019-02-05 18:00:40 +0000

    new: Add attribute and event creation

--- lib/misp.ex
+++ lib/misp.ex
@@ -65,7 +65,20 @@ defmodule MISP do
     end
   end
 
-  def limit_allowed_search_terms(%{} = params, allowed_attributes) do
+  def enforce_mandatory_keys(%{} = params, mandatory_attributes) do
+    not_included =
+      mandatory_attributes
+      |> Enum.filter(fn x -> not Map.has_key?(params, x) end)
+    
+    unless Enum.count(not_included) > 0 do
+      params
+    else 
+      error_msg = Enum.join(not_included, ",")
+      raise ArgumentError, "Required parameters missing: #{error_msg}"
+    end
+  end
+
+  defp limit_keys_to_permitted(%{} = params, allowed_attributes) do
     allowed_attributes
     |> Enum.reduce(
         %{},
@@ -90,10 +103,27 @@ defmodule MISP do
   end
 
   @doc """
-  Search MISP at the index level. Query values can be negated by prepending an
-  exclamation mark (!)
+  Interact with restSearch endpoints
 
-    iex> MISP.search(%{published: true, eventid: 5})
+    iex> MISP.search("attributes", %{"value" => "8.8.8.8", "type" => "ip-dst"})
+    [
+      %{
+        "Event" => %{
+        },
+        "category" => "Network activity",
+        "event_id" => "someId",
+        "type" => "ip-dst",
+        "value" => "8.8.8.8"
+      },
+    ]
+
+    iex> MISP.search("events", %{"eventid" => "12345"})
+    [
+      %{
+        "Event" => %{
+        }
+      }
+    ]
   """
   def search("attributes", %{} = params) do
     search_base = %{
@@ -111,10 +141,12 @@ defmodule MISP do
     to_post = 
       search_base
       |> Map.merge(params)
-      |> limit_allowed_search_terms(allowed_attributes)
+      |> limit_keys_to_permitted(allowed_attributes)
 
     config()
     |> post("/attributes/restSearch", to_post)
+    |> Map.get("response")
+    |> Map.get("Attribute")
   end
 
   def search("events", %{} = params) do
@@ -134,9 +166,97 @@ defmodule MISP do
     to_post =
       search_base
       |> Map.merge(params)
-      |> limit_allowed_search_terms(allowed_attributes)
+      |> limit_keys_to_permitted(allowed_attributes)
 
     config()
     |> post("/events/restSearch", to_post)
+    |> Map.get("response")
+  end
+
+  def search(index, _) do
+    {:error, reason: "Invalid index #{index}"}
+  end
+
+  def get_event(id) when is_integer(id) or is_binary(id) do
+    config()
+    |> get("/events/#{id}")
+  end
+
+  def get_event(%{"Event" => _} = event) do
+   event
+  end
+
+  def create_event(%{} = params) do
+    mandatory_attributes = ["info"]
+    allowed_attributes = [
+      "info", "threat_level_id", "analysis", "distribution", "sharing_group_id",
+      "uuid", "published", "timestamp", "date", 
+    ]
+
+    to_post = 
+      params
+      |> enforce_mandatory_keys(mandatory_attributes)
+      |> limit_keys_to_permitted(allowed_attributes)
+
+    config()
+    |> post("/events/add", to_post)
+  end
+
+  defp append_new_attribute(%{} = attribute, %{"Event" => %{"Attribute" => attr_list}} = event) do
+    new_attr_list = 
+      attr_list
+      |> List.insert_at(0, attribute)
+
+    new_event =
+      event
+      |> Map.get("Event")
+      |> Map.put("Attribute", new_attr_list)
+
+    event
+    |> Map.put("Event", new_event)
+  end
+
+  @doc """
+  Create a new attribute
+
+    iex> event_id = 955
+    iex> MISP.create_attribute(event_id, %{"type" =>" ip-dst", "value" => "8.8.8.8"})
+    %{
+      "Event" => %{
+        "Attribute" => [
+          %{
+            "type" => "ip-dst",
+            "value" => "8.8.8.8"
+          }
+        ]
+      }
+    }
+
+  Can also be piped with create_event
+
+    iex> MISP.create_event(%{"info" => "my event"})
+         |> MISP.create_attribute(%{"type" =>" ip-dst", "value" => "8.8.8.8"})
+  """
+  def create_attribute(event, %{} = params) do
+    mandatory_attributes = ["value", "type"]
+    allowed_attributes = [
+      "value", "type", "category", "to_ids",
+      "uuid", "distribution", "sharing_group_id",
+      "timestamp", "comment"
+    ]
+
+    to_post =
+      params
+      |> enforce_mandatory_keys(mandatory_attributes)
+      |> limit_keys_to_permitted(allowed_attributes)
+
+    %{"Event" => %{"id" => event_id}} =
+      event
+      |> get_event()
+
+    config()
+    |> post("/attributes/add/#{event_id}", to_post)
+    |> Map.get("Attribute")
+    |> append_new_attribute(event)
   end
 end
